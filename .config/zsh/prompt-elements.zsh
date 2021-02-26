c87bf8788813029aea9afd2bc40b3075f3555c4c
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-02-26 08:39:16 -0500

    Remove prompt elements file as it is no longer needed

--- .config/zsh/prompt-elements.zsh
+++
@@ -1,7 +0,0 @@
-function prompt_conditional_newline() {
-  # detect is newline needs to be added
-  if git rev-parse --git-dir > /dev/null 2>&1; then
-    # add newline into prompt
-    p10k segment -t "\n"
-  fi
-}
