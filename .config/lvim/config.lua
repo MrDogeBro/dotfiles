5a7c0045ec2f3ea70226cb74318ce93847bfd110
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-12-02 17:42:39 -0500

    Use relative line numbers

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -77,6 +77,16 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
+-- ==============================================
+--                Custom Prefs
+-- ==============================================
+
+vim.opt.relativenumber = true;
+
+-- ==============================================
+--                Extra Config
+-- ==============================================
+
 -- generic LSP settings
 
 -- ---@usage disable automatic installation of servers
