231158c333e462f85d0790a92def7a71ac689ef2
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-08-29 17:03:20 -0400

    Update config for lunarvim updates

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -59,7 +59,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -151,7 +151,7 @@ vim.opt.relativenumber = true
 -- vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprev<CR>', { noremap = true, silent = true })
 
-lvim.lsp.automatic_servers_installation = true
+lvim.lsp.installer.setup.automatic_installation = true
 
 -- keybinds
 lvim.builtin.which_key.mappings["w"] = {
